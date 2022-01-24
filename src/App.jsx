7a1c5348ca2c1725bd95d71c051d2bc94a711881
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2022-01-24 16:40:25 +0100

    table of jobs

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import JobList from './pages/Joblist'
 import Detail from './pages/Details';
-import { Container, Row, Col, Form, Button } from 'react-bootstrap'
-import {useState} from 'react'
+import { Container, Row, Col, Form, Button, Table } from 'react-bootstrap'
+import {useState, useEffect} from 'react'
+
 
 function App() {
 const [jobs, setJobs]= useState([])
@@ -11,12 +12,27 @@ const [jobs, setJobs]= useState([])
 
 const getJobs = async () =>{
 
+    try {
+       const res = await fetch('https://strive-jobs-api.herokuapp.com/jobs')
+   if(res.ok){
+   console.log(res)
+   const data = await res.json()
+   setJobs(data)
+}else{
+   console.log("something goes wrong with data")
 }
+} catch (error) {
+   console.log("server error")
+}
+}    
 
 
+useEffect(()=>{  
+    
+getJobs()        
+}, [])
 
-
-    return (
+   return (
         <BrowserRouter>
         
       <Container fluid className="text-center">
@@ -24,13 +40,13 @@ const getJobs = async () =>{
           <hr />
         <Row style={{display:"flex"}}>
           <Col sm={12}  className='text-center' style={{marginRight:"3rem", marginTop:"3rem"}}>
-          <Form inline>
+          <Form onChange={(e)=>console.log(e.target.value)}>
  
   <Form.Control
     className="mb-2 mr-sm-2"
     id="search"
     placeholder="search"
-    onSubmit={()=>getJobs()}
+    
   />
   
   <Button type="submit" className="mb-2">
@@ -39,7 +55,20 @@ const getJobs = async () =>{
 </Form>
           </Col>
           <Col sm={12} className='text-center'>
-            <JobList />
+          <Table striped bordered hover>
+  <thead>
+    <tr>
+      
+      <th>Title</th>
+      <th>Company</th>
+      <th>Detail</th>
+    </tr>
+  </thead>
+  
+    <JobList jobs={jobs}/>
+
+</Table>
+            
           </Col>
         </Row>
  
--- src/pages/Joblist.jsx
+++ src/pages/Joblist.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Joblist =  ()=>{
+
+
+const Joblist = ({jobs})=>{
+     console.log("this is jobs in JobList array", jobs.data)
      return (
-     <h1>Job list</h1>
-     )}
+          <>
+     
+     <div>
 
+
+     {jobs.data.slice(0,10).map((j, i)=>
+     <tbody>
+     <tr key={i}>
+     
+     <td>{j.title}</td>
+     <td>{j.company_name}</td>
+     <Link >
+     <td>detail</td></Link>
+   </tr>
+          </tbody>
+          
+          )}
+          </div>
+          </>
+     )
+     }
 
      export default Joblist
