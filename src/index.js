7bb895fa0377466fff0e87c8868e831367736595
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2022-01-31 16:59:33 +0100

    fixed bug

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import { Provider } from 'react-redux'
 import { configureStore, persistor } from '../src/redux/store'
 import { PersistGate } from 'redux-persist/integration/react'
 
-ReactDOM.render( < React.StrictMode >
+ReactDOM.render( <
+    React.StrictMode >
     <
     Provider store = { configureStore } >
     <
@@ -14,8 +15,10 @@ ReactDOM.render( < React.StrictMode >
     <
     App / >
     <
-    /Provider>  </PersistGate > <
-    /
-    React.StrictMode > ,
+    /PersistGate >  <
+    /Provider>  
+
+    <
+    /React.StrictMode> ,
     document.getElementById('root')
 );
