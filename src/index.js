406f3b99540b9e89ee59db94c49955618d9fcc50
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2022-01-31 16:57:20 +0100

    cleaning code

--- src/index.js
+++ src/index.js
@@ -3,17 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.jsx';
 import { Provider } from 'react-redux'
-import configureStore from '../src/redux/store'
+import { configureStore, persistor } from '../src/redux/store'
+import { PersistGate } from 'redux-persist/integration/react'
 
-
-ReactDOM.render( <
-    React.StrictMode >
+ReactDOM.render( < React.StrictMode >
     <
     Provider store = { configureStore } >
     <
+    PersistGate persistor = { persistor } >
+    <
     App / >
     <
-    /Provider> < /
+    /Provider>  </PersistGate > <
+    /
     React.StrictMode > ,
     document.getElementById('root')
 );
